452a400ed549fb0e96e5a6e795e29ecac585b908
talha kayani <talha.kayani@invozone.com>
2021-09-02 18:18:19 +0500

    deploying Heroku

--- app.js
+++ app.js
@@ -12,33 +12,7 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/', userRoute);
 app.use('/', exerciseRoute);
-// if(!sequelize){
-//     sequelize = new Sequelize({
-//       database: ,
-//       username: "[won't show username]",
-//       password: "[won't show password]",
-//       host: 'ec2-54-221-195-148.compute-1.amazonaws.com',
-//       port: 5432,
-//       dialect: 'postgres',
-//       dialectOptions: {
-//         ssl: {
-//           require: true,
-//           rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
-//         },
-//       },
-//     });
-// }
 app.listen(PORT, async () => {
   console.log(sequelize);
   await sequelize.authenticate();
 });
-// app.listen(PORT, async () => {
-//   console.log('Connecting Database...');
-//   await sequelize.authenticate();
-//   console.log(`Server Running: http://localhost:${PORT}/api-docs`);
-// });
-// , async () => {
-// console.log('Connecting Database...');
-// await sequelize.authenticate();
-// console.log(`Server Running: http://localhost:${PORT}/api-docs`);
-// });
