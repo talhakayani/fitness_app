234e9a7dba7fe428c0aafc4da4505f85c5a1a3b4
talha kayani <talha.kayani@invozone.com>
2021-09-02 17:33:20 +0500

    working

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('./swagger.json');
-const { Sequelize } = require('./models');
+const { sequelize } = require('./models');
 const userRoute = require('./routes/userRoute');
 const exerciseRoute = require('./routes/exerciseRoute');
+const Sequelize = require('sequelize');
 
 const PORT = process.env.PORT || 3000; //5432; //process.env.PORT || 5432;
 const app = express();
@@ -12,15 +13,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/', userRoute);
 app.use('/', exerciseRoute);
 
-sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: true,
-  },
-});
-
 app.listen(PORT, () => {
+  console.log(sequelize);
   sequelize.authenticate();
 });
 // app.listen(PORT, async () => {
